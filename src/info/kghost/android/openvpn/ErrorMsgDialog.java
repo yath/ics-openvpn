ad71f55892cb5b330c9626df95df860e7224f86f
Sebastian Schmidt <yath@yath.de>
2012-04-22 10:28:53 +0200

    Fix ErrorMsgDialog
    Return the Dialog in onCreateDialog instead of onCreate; also use
    getString for translating the string resource into a CharSequence.

--- src/info/kghost/android/openvpn/ErrorMsgDialog.java
+++ src/info/kghost/android/openvpn/ErrorMsgDialog.java
@@ -4,6 +4,7 @@ import java.util.concurrent.Callable;
 
 import android.app.AlertDialog;
 import android.app.DialogFragment;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 
@@ -17,9 +18,10 @@ public class ErrorMsgDialog extends DialogFragment {
 	}
 
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		new AlertDialog.Builder(getActivity())
-				.setTitle(R.string.openvpn_install_error_title).setMessage(msg)
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+		return new AlertDialog.Builder(getActivity())
+				.setTitle(getString(R.string.openvpn_install_error_title)).setMessage(msg)
 				.setCancelable(false)
 				.setPositiveButton("OK", new DialogInterface.OnClickListener() {
 					public void onClick(DialogInterface dialog, int id) {
